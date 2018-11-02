1f1fb80977fcf39af54e2fa6af3d78fdccd1fd9e
Oliver Breidenbach <oliver.breidenbach@xima.de>
2018-11-02 15:31:57 +0100

    #4196, Testcase

--- src/main/java/org/primefaces/test/TestViewCdi.java
+++ src/main/java/org/primefaces/test/TestViewCdi.java
@@ -1,7 +1,13 @@
 package org.primefaces.test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.RequestScoped;
+import javax.faces.model.SelectItem;
 import javax.inject.Named;
 
 @Named
@@ -9,10 +15,45 @@ import javax.inject.Named;
 public class TestViewCdi {
     
     private String testString;
+
+    List<SelectItem> options;
+    List<String> selected = new ArrayList<>();
     
     @PostConstruct  
     public void init() {
         testString = "Welcome to PrimeFaces (from CDI!) !!!";
+
+
+        options = new LinkedList<>();
+        for(char c = 'A'; c <= 'Z'; c++) {
+            char[] repeat = new char[3];
+            Arrays.fill(repeat, c);
+            String value = new String(repeat).intern();
+            options.add(new SelectItem(value));
+            selected.add(value);
+        }
+        for(char c = 'a'; c <= 'z'; c++) {
+            char[] repeat = new char[3];
+            Arrays.fill(repeat, c);
+            String value = new String(repeat).intern();
+            options.add(new SelectItem(value));
+        }
+    }
+
+    public List<SelectItem> getOptions() {
+        return options;
+    }
+
+    public void setOptions(List<SelectItem> options) {
+        this.options = options;
+    }
+
+    public List<String> getSelected() {
+        return selected;
+    }
+
+    public void setSelected(List<String> selected) {
+        this.selected = selected;
     }
 
     public String getTestString() {
