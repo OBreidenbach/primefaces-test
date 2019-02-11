2500c3808b3bc0d0870f6f11b067998f6bd578f4
Oliver Breidenbach <oliver.breidenbach@xima.de>
2019-02-11 10:36:19 +0100

    #4524, test case

--- src/main/java/org/primefaces/test/TestViewCdi.java
+++ src/main/java/org/primefaces/test/TestViewCdi.java
@@ -1,5 +1,7 @@
 package org.primefaces.test;
 
+import java.util.ArrayList;
+import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Named;
@@ -9,10 +11,18 @@ import javax.inject.Named;
 public class TestViewCdi {
     
     private String testString;
-    
+
+    private List<String> list;
+
     @PostConstruct  
     public void init() {
         testString = "Welcome to PrimeFaces (from CDI!) !!!";
+
+        List<String> list = new ArrayList<>();
+        for (int i = 1; i <= 10; i++) {
+            list.add("Text " + i);
+        }
+        this.list = list;
     }
 
     public String getTestString() {
@@ -21,5 +31,9 @@ public class TestViewCdi {
 
     public void setTestString(String testString) {
         this.testString = testString;
-    }    
+    }
+
+    public List<String> getList() {
+        return list;
+    }
 }
