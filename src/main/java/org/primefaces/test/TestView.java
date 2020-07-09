5f69e9dac5187f069b0f6d77427b195e326e7aa6
Oliver Breidenbach <oliver.breidenbach@xima.de>
2020-07-09 09:56:17 +0200

    Showing bug for DataTable with paginator and sortBy

--- src/main/java/org/primefaces/test/TestView.java
+++ src/main/java/org/primefaces/test/TestView.java
@@ -1,6 +1,8 @@
 package org.primefaces.test;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
@@ -8,12 +10,18 @@ import javax.inject.Named;
 @Named
 @ViewScoped
 public class TestView implements Serializable {
-    
+
     private String testString;
-    
-    @PostConstruct  
+    private List<Row> rows;
+
+    @PostConstruct
     public void init() {
         testString = "Welcome to PrimeFaces!!!";
+        List<Row> _list = new ArrayList<>(50);
+        for (int i = 0; i < 50; i++) {
+            _list.add(new Row(i));
+        }
+        rows = _list;
     }
 
     public String getTestString() {
@@ -22,5 +30,28 @@ public class TestView implements Serializable {
 
     public void setTestString(String testString) {
         this.testString = testString;
-    }    
+    }
+
+    public List<Row> getRows() {
+        return rows;
+    }
+
+    public static class Row implements Serializable{
+        private String str;
+
+        public Row() {
+        }
+
+        public Row(int i) {
+            this.str = String.format("row %02d", i);
+        }
+
+        public String getStr() {
+            return str;
+        }
+
+        public void setStr(String str) {
+            this.str = str;
+        }
+    }
 }
